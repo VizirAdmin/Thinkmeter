689b851c444229dfa9c411eb05a6af21944d3ed8
Andre <andre0610@gmail.com>
2010-10-15 23:46:08 -0300

    Search worker implemented

--- app/jobs/search_worker.rb
+++ app/jobs/search_worker.rb
@@ -10,5 +10,25 @@ class SearchWorker
     @twitter.search("#IThink")
   end
 
+  def add_search_to_database
+    search = Search.first
+    page = get_tweets(search.last_tweet_id)
+    total = 0
+    if (!page.nil?) && (!page.first.nil?)
+      search.last_tweet_id = page.first.id
+      search.save
+    end
+    while !page.nil? and page.size>0 do
+      total += page.size
+#      add_page_to_database(page).each do |r|
+
+#      end
+      page=@twitter.go_to_next_page
+    end
+    return total
+  end
+
+
+
 end
 
