59cbfa6c2ee6c38badaf9d855ef3f718830a4114
Andre <andre0610@gmail.com>
2010-10-15 23:58:42 -0300

    Message model was added

--- app/jobs/search_worker.rb
+++ app/jobs/search_worker.rb
@@ -20,15 +20,26 @@ class SearchWorker
     end
     while !page.nil? and page.size>0 do
       total += page.size
-#      add_page_to_database(page).each do |r|
-
-#      end
+      add_page_to_database(page)
       page=@twitter.go_to_next_page
     end
     return total
   end
 
+    def add_message_to_database(msg)
+    message = Message.create_and_check(:from_user=>msg.from_user, :from_user_id=>msg.from_user_id,
+                :geo=>msg.geo, :twitter_id=>msg.id, :language_code=>msg.iso_language_code,
+                :profile_img_url=>msg.profile_image_url, :source =>msg.source,
+                :text=>msg.text, :to_user_id=>msg.to_user_id, :created_at=>msg.created_at,
+                :recent_retweets =>msg.metadata.recent_retweets, :result_type=>msg.metadata.result_type,
+                :status => 0)
+   end
 
+  def add_page_to_database(page)
+    page.each do |msg|
+      db_msg  =  add_message_to_database(search,msg)
+    end
+  end
 
 end
 
