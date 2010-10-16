d2c7ae07f72714f5041957e1389cb6155d7839c3
Antonio Anderson Souza <antonioams@gmail.com>
2010-10-16 00:41:32 -0300

    Model Opinion e fixture preenchendo opiniões basicas em português

--- db/migrate/20101016025543_create_messages.rb
+++ db/migrate/20101016025543_create_messages.rb
@@ -18,8 +18,6 @@ class CreateMessages < ActiveRecord::Migration
       t.timestamps
     end
     execute("ALTER TABLE messages MODIFY COLUMN twitter_id BIGINT NOT NULL")
-    t.timestamps
-    end
   end
 
   def self.down
