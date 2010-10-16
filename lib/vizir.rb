3e67de554166cf590714d791db865cff0b4e6acd
Andre <andre0610@gmail.com>
2010-10-15 23:05:09 -0300

    Twitter Search class included

---
+++ lib/vizir.rb
@@ -0,0 +1,47 @@
+module Vizir
+  class Twitter
+    @next_page_url, @last_query = "",""
+    def search(search_string, last_tweet_id)
+      result = Restfulie.at("http://search.twitter.com/search.json").accepts("application/json").get(:q=>search_string,:rpp=>"100", :since_id=>"#{last_tweet_id}")
+      @last_query=search_string
+      set_next_page_and_results(result)
+    end
+
+    def set_next_page_and_results(result)
+      if result.class==Array
+        @next_page_url=nil
+        nil
+      else
+        @next_page_url = result.next_page
+        result.results
+      end
+    end
+
+    def go_to_next_page
+      if @next_page_url.nil? || @next_page_url.class==Array
+        nil
+      else
+        result = Restfulie.at("http://search.twitter.com/search.json").accepts("application/json").get(build_params(@next_page_url[1..-1],@last_query,"100"))
+        set_next_page_and_results(result)
+      end
+    end
+
+    def get_twitter_profile(profile)
+      Restfulie.at("http://api.twitter.com/1/users/show/#{profile}").accepts("application/json").get()
+    end
+
+private
+  def build_params(next_page,query,rpp)
+    hash_params = {}
+    params = next_page.split("&")
+    params.each do |p|
+      op = p.split("=")
+      hash_params[op[0]]=op[1]
+    end
+    hash_params["q"]=query
+    hash_params["rpp"]=rpp
+    hash_params
+  end
+end
+end
+
