1774409b527d796f592956ec173e4f6950c2f9ad
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2010-10-17 05:43:35 -0200

    #update arrumada parte de randomizar o nome da marca na questão

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @brands = Brand.find_all_with_tags
-    @brand_name = @brands.sort[0][:tag]
+    @brand_name = @brands[rand(@brands.size)][:tag]
     @opinions_positives = Opinion.find_all_with_tags(:context => Opinion::GOOD)
     @opinions_negatives = Opinion.find_all_with_tags(:context => Opinion::BAD)
     @messages = Message.last_messages
