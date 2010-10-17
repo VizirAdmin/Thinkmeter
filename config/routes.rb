50d524a7264303c86a2771eca5522e724b0b9e27
Marcos Hack <marcoshack@gmail.com>
2010-10-17 16:29:02 -0200

    Alteracao da rota para exibicao da Opinion
    era "opinions/:id/opinion_page" e virou "opinions/:id"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home", :action => "index"
+  
   map.brand_validation '/brand_validation', :controller => 'brands_status', :action => 'index'
   map.brand_messages '/brands/:id/messages' , :controller => 'brands', :action => 'messages'
   map.brand_page '/brands/:id/brand_page' , :controller => 'brands', :action => 'brand_page'
-  map.opinion_page '/opinions/:id/opinion_page' , :controller => 'opinions', :action => 'opinion_page'
+  
+  #map.opinion '/opinions/:id/opinion' , :controller => 'opinions', :action => 'opinion'
+  #map.opinion '/ccccc', :controller => 'opinions', :action=> 'index', :opinion=> 'aaa'
+  
   map.resources :brands
-  map.opinion '/ccccc', :controller => 'opinions', :action=> 'index', :opinion=> 'aaa'
   map.resources :opinions
   map.resources :expressions
+  
   map.connect ':controller/:action'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
